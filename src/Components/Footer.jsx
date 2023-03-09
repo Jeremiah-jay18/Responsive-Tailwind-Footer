707aa262e08bfac1cb5baa1cd71668ad325d5162
Jeremiah-jay18 <111522532+Jeremiah-jay18@users.noreply.github.com>
2023-03-09 04:36:20 -0800

    Update Footer.jsx

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -28,28 +28,28 @@ function toggleAccordion(i){
 }
 
 const boxTwo= info.map((item,i)=>{
-    return <div key={item.id} className='sm:mt-3 border border-l-black border-r-black sm:-mx-3'>
+    return <div key={item.id} className='sm:mt-3 border border-l-neutral-900 border-r-neutral-900 sm:-mx-3'>
       <div className={`flex justify-between cursor-pointer border-left-black content-center sm:pt-2`} onClick={()=>toggleAccordion(i)}  >
-         <h2 className='font-medium text-xl mb-4 sm:ml-4'>{item.title}</h2>
-         {selected===i?<IoIosArrowUp size={26} />:<IoIosArrowDown size={26} />}
+         <h2 className='font-medium text-xl mb-4 sm:ml-4 sm:text-sm sm:font-thin'>{item.title}</h2>
+         {selected===i?<IoIosArrowUp size={20} />:<IoIosArrowDown size={20} />}
       </div>
-             <div className={`px-4 ${selected===i?'overflow-hidden h-48 transition-all duration-2000 ease-linear':' overflow-hidden h-0 transition-all duration-2000 ease-linear'}`}>
+             <div className={`px-6 ${selected===i?'h-48 transition-all duration-2000 ease-linear overflow-hidden':' overflow-hidden h-0 transition-all duration-2000 ease-linear'}`}>
                 {item.data.map((item,i)=>{
-                    return <Link className='block mt-3' key={i}>{item}</Link>
+                    return <Link className=' text-sm block mt-3' key={i}>{item}</Link>
                })
                 }
              </div>
         </div>
 })
   return (
-    <div className='bg-black text-white'>
+    <div className='bg-neutral-900 text-white'>
 {/* General Blocks Wrapper */}
         <div className="foot-wrapper lg:flex lg:justify-between sm:block ">
 {/* Between Valdymas block and Info block */}
       <div className="sm:px-5 px-20 py-16 flex justify-between lg:gap-10 sm:block sm:py-6 ">
 
         <div className="box-1 lg:-mt-5 lg:h-20 sm:text-center">
-        <h1 className='mb-3 footer-head text-2xl'>VALDYMAS</h1>
+        <h1 className='sm:text-lg mb-3 footer-head text-2xl'>VALDYMAS</h1>
     <div className="sm:hidden flex justify-between">
 <FaFacebookSquare size={26} className="cursor-pointer"/>
  <FaTwitterSquare  size={26} className="cursor-pointer"/>
@@ -68,38 +68,38 @@ const boxTwo= info.map((item,i)=>{
 
  <div className=" form ">
     <form action=''>
-        <div className="flex gap-4 mb-10 sm:justify-center">
+        <div className="flex gap-4 mb-10 sm:justify-center items-center">
             <MdEmail size={26}/>
-            <h2 className='text-md font-medium'>Stay Up To Date On The Latest From Valdymas</h2>
+            <h2 className='text-md font-medium sm:text-sm'>Stay Up To Date On The Latest From Valdymas</h2>
         </div>
         <div className="flex gap-6 mb-4">
-            <input type="text" className=" py-3 placeholder-gray-500 bg-black border sm:w-full
-            border-b-white border-t-black  border-r-black border-l-black outline-none " placeholder='First Name ' />
-            <input type="text" className=" py-3 placeholder-gray-500 bg-black border sm:w-full
-             border-b-white border-t-black  border-r-black border-l-black
+            <input type="text" className=" bg-neutral-900 py-3 placeholder-gray-500 bg-black border sm:w-full
+            border-b-white border-t-neutral-900  border-r-neutral-900 border-l-neutral-900 outline-none " placeholder='First Name ' />
+            <input type="text" className="bg-neutral-900 py-3 placeholder-gray-500 bg-black border sm:w-full
+             border-b-white border-t-neutral-900  border-r-neutral-900 border-l-neutral-900
             shadow-md outline-none " placeholder='Last Name'/>
         </div>
-        <input type="email" className=" py-2 placeholder-gray-500 bg-black shadow-md border 
-         border-b-white border-t-black  border-r-black border-l-black outline-none w-full" placeholder='Email'/>
-         <div className='py-3 border border-t-black  border-r-black border-l-black'>
+        <input type="email" className="bg-neutral-900 py-2 placeholder-gray-500 bg-black shadow-md border 
+         border-b-white border-t-neutral-900  border-r-neutral-900 border-l-neutral-900 outline-none w-full" placeholder='Email'/>
+         <div className='py-3 border border-t-neutral-900  border-r-neutral-900 border-l-neutral-900'>
          <p>
           <label htmlFor="agree">
-         <input id='agree'type="checkbox" className='mr-2'/>I have read and accepted the school policy
+         <input id='agree'type="checkbox" className='mr-2'/><span className="sm:text-xs">I have read and accepted the <Link className='underline' to =''>school policy</Link></span>
         </label>
         </p>
          </div>
 
          <div className="sm:text-center ">
-         <input type="submit" value="Sign Up" className='sm:w-full bg-red-700 py-3 cursor-pointer px-20 mt-3 ' />
+         <input type="submit" value="Sign Up" className='sm:w-full bg-red-800 py-3 cursor-pointer px-20 mt-3 sm:text-sm' />
          </div>
     </form>
  </div>
 
 
     <div className="foot-icons flex gap-4 lg:hidden justify-center my-4">
-<FaFacebookSquare size={30} className="cursor-pointer"/>
- <FaTwitterSquare  size={30} className="cursor-pointer"/>
- <FaYoutubeSquare  size={30} className="cursor-pointer"/>
+<FaFacebookSquare size={25} className="cursor-pointer"/>
+ <FaTwitterSquare  size={25} className="cursor-pointer"/>
+ <FaYoutubeSquare  size={25} className="cursor-pointer"/>
         </div>
 
  </div>
@@ -109,7 +109,9 @@ const boxTwo= info.map((item,i)=>{
 </div>
 
 <hr />
-<div className='py-5 px-4 footer-foot'>@2023 Valdymas College. All Rights Reserved</div>
+<div className='sm:text-sm py-5 px-4 footer-foot'>@2023 Valdymas. <span className='block'>
+All Rights Reserved
+  </span></div>
 
 
 
